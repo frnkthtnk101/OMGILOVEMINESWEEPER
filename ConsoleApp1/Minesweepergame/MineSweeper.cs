a8e72b607568f1126997fdbbf6f92043f3efea90
Franco Pettigrosso <fpettigrosso@holyfamily.edu>
2018-12-13 15:28:43 -0500

    make minesweepergame

---
+++ ConsoleApp1/Minesweepergame/MineSweeper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Minesweepergame
+{
+    class MineSweeper
+    {
+        byte[,] _Map;
+        byte _NumOfBombs;
+        byte _size;
+
+        public MineSweeper(byte Size, byte BombCount)
+        {
+            _size = Size;
+            _Map = new byte[_size, _size];
+            _NumOfBombs = BombCount;
+            int limit = _size - 5;
+            int i = 0;
+            for (; i < limit; i += 5)
+                for (int j = 0; j < _size; j += 1)
+                {
+                    _Map[i, j] = 0;
+                    _Map[i + 1, j] = 0;
+                    _Map[i + 2, j] = 0;
+                    _Map[i + 3, j] = 0;
+                    _Map[i + 4, j] = 0;
+                }
+            for (; i < _size; i++)
+                for (int j = 0; j < _size; j += 1)
+                {
+                    _Map[i, j] = 0;
+                }
+            _SetBombs();
+        }
+
+        public bool PickSpot(int x, int y)
+        {
+            if(x != -1)
+                if(_Map[y,x] == 1)
+                {
+                    return false;
+                }
+            return true;
+        }
+
+        private void _SetBombs()
+        {
+            Random random = new Random();
+            byte PlacedBombs = 0;
+            do
+            {
+                int x = random.Next(0, _size);
+                int y = random.Next(0, _size);
+                if(_Map[y,x] == 0)
+                {
+                    _Map[x, y] = 1;
+                    _NumOfBombs++;
+                }
+            } while (PlacedBombs < _NumOfBombs);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder @string = new StringBuilder();
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j += 1)
+                {
+                    @string.Append($"{_Map[i, j]}");
+                }
+                @string.Append("\r\n");
+            }
+            return @string.ToString();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Minesweepergame;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"([\d]{0,3})");
+            Minesweepergame.MineSweeper game = new Minesweepergame.MineSweeper(25,5);
+            Console.WriteLine(game.ToString());
+            int[] cords = new int[2];
+            do
+            {
+                Console.WriteLine("pick an x,y");
+                string input = Console.ReadLine();
+                if (regex.IsMatch(input))
+                {
+                    MatchCollection capture = regex.Matches(input);
+                    cords[0] = int.Parse(capture[0].Value);
+                    cords[0] = int.Parse(capture[1].Value);
+                }
+                else
+                {
+                    cords[0] = -1;
+                    cords[1] = -1;
+                }
+
+
+
+            } while (game.PickSpot(cords[0], cords[1]));
+            Console.WriteLine("Game over!");
+            Console.ReadLine();
+        }
+    }
+}
